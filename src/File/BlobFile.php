72aa0ff9a52c444b7d395ebedf448e989639968e
Josef Panák <josef.panak@ringieraxelspringer.sk>
2017-10-10 20:45:52 +0200

    add FileInterface, BlobFile change FileClient upload method, File paramter change to FileInterface

---
+++ src/File/BlobFile.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MogileFs\File;
+
+use RuntimeException;
+
+class BlobFile implements FileInterface
+{
+    private $payload;
+    private $class;
+
+    public function __construct($payload, $class)
+    {
+        $this->payload = $payload;
+        $this->class = $class;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStream()
+    {
+        $fh = fopen('php://memory', 'rw');
+        if ($fh === false) {
+            throw new RuntimeException(get_class($this) . "::getStream() failed to open memory stream");
+        }
+        fwrite($fh, $this->payload);
+        rewind($fh);
+        $length = strlen($this->payload);
+        dump($length);
+        return [$fh, $length];
+    }
+}
